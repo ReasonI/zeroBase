90f2230683c7a06b7f56db904e676faed4cb4b0d
UNA <lemonlee030@gmail.com>
2023-11-25 15:36:13 +0900

    create

---
+++ src/main/java/com/example/pj01account/Pj01AccountApplication.java
@@ -0,0 +1,13 @@
+package com.example.pj01account;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pj01AccountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pj01AccountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pj01account/Pj01AccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pj01account;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pj01AccountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
