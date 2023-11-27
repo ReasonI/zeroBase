e6bdde8669d798609f577dc7938d8316643c72b0
UNA <lemonlee030@gmail.com>
2023-11-27 18:50:30 +0900

    redis test

--- src/main/java/com/example/pj01account/controller/AccountController.java
+++ src/main/java/com/example/pj01account/controller/AccountController.java
@@ -2,6 +2,7 @@ package com.example.pj01account.controller;
 
 import com.example.pj01account.domain.Account;
 import com.example.pj01account.service.AccountService;
+import com.example.pj01account.service.RedisTestService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountController {
 
     private final AccountService accountService;
+    private final RedisTestService redisTestService;
+
+    @GetMapping("/get-lock")
+    public String getLock(){
+        return redisTestService.getLock();
+    }
 
     @GetMapping("/create-account")
     public String createAccount(){
