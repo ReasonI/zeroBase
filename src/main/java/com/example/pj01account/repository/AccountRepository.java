5d2eab3c558b621ba1f76e1ad159b48d79097069
UNA <lemonlee030@gmail.com>
2023-11-29 17:49:31 +0900

    create validation

--- src/main/java/com/example/pj01account/repository/AccountRepository.java
+++ src/main/java/com/example/pj01account/repository/AccountRepository.java
@@ -1,15 +1,19 @@
 package com.example.pj01account.repository;
 
 import com.example.pj01account.domain.Account;
+import com.example.pj01account.domain.AccountUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findFirstByOrderByIdDesc();
+
+    Integer countByAccountUser(AccountUser accountUser);
 
     Optional<Account> findByAccountNumber(String AccountNumber);
 }
--- src/main/java/com/example/pj01account/service/AccountService.java
+++ src/main/java/com/example/pj01account/service/AccountService.java
@@ -16,6 +16,7 @@ import java.time.LocalDateTime;
 import java.util.Objects;
 
 import static com.example.pj01account.type.AccountStatus.IN_USE;
+import static com.example.pj01account.type.ErrorCode.MAX_ACCOUNT_PER_USER_10;
 
 @Service
 @RequiredArgsConstructor
@@ -35,6 +36,8 @@ public class AccountService {
         AccountUser accountUser = accountUserRepository.findById(userId)
                 .orElseThrow(() -> new AccountException(ErrorCode.USER_NOT_FOUND));
 
+        validateCreateAccount(accountUser);
+
         String newAccountNumber = accountRepository.findFirstByOrderByIdDesc()
                 .map(account -> (Integer.parseInt(account.getAccountNumber())) + 1 + "")
                 .orElse("1000000000");
@@ -50,8 +53,17 @@ public class AccountService {
                 ));
     }
 
+    private void validateCreateAccount(AccountUser accountUser) {
+        if(accountRepository.countByAccountUser(accountUser) == 10){
+            throw new AccountException(ErrorCode.MAX_ACCOUNT_PER_USER_10);
+        }
+    }
+
     @Transactional
     public Account getAccount(Long id) {
+        if(id < 0){
+            throw new RuntimeException("Minus");
+        }
         return accountRepository.findById(id).get();
     }
 
